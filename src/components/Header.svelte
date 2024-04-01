4fea4607b226f7c100853c377786885e242f0e4a
Echoooo <echoshihtw@gmail.com>
2024-04-01 15:12:14 +0800

    fix: scroll position

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import ThemeSwitch from "./ThemeSwitch.svelte";
 
-  export let y: number;
+  export let scrollPosition: number;
 
   let tabs = [
     { name: "Projects", link: "#projects" },
@@ -13,8 +13,8 @@
 
 <header
   class={"w-full sticky z-[10] top-0 duration-200 px-6 border-b border-solid " +
-    (y > 60
-      ? "py-4 bg-slate-950 border-violet-950"
+    (scrollPosition > 60
+      ? "py-4 bg-slate-950 border-lime-950"
       : "py-6 bg-transparent border-transparent")}
 >
   <div class={"w-full max-w-[1400px] flex items-center justify-between m-auto"}>
@@ -22,21 +22,21 @@
       <b>Echo</b>
       Shih
     </h1>
-    <div class="hidden sm:flex items-center gap-4">
+    <div class="flex items-center gap-4">
       {#each tabs as tab, index}
         <a
           href={tab.link}
-          class="duration-200 hover:text-violet-400"
+          class="duration-200 toggle_hover_font_color"
           target={index === 3 ? "_blank" : ""}
         >
           <p>{tab.name}</p>
         </a>
       {/each}
       <button
-        class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white text-slate-950"
+        class="dark:blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white text-slate-950"
       >
         <span
-          class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
+          class="absolute top-0 right-full w-full h-full bg-lime-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
         />
         <span class="relative z-9">Get in touch</span>
       </button>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,10 +3,23 @@
   import Icon from "@iconify/svelte";
   import Header from "../components/Header.svelte";
   import Footer from "../components/Footer.svelte";
+  import { onMount } from "svelte";
 
-  let y: number;
-  let innerWidth: number = 0;
-  let innerHeight: number = 0;
+  let scrollPosition: number;
+
+  // Function to be called whenever the user scrolls
+  function handleScroll() {
+    scrollPosition = window.scrollY; // Get the current vertical scroll position
+  }
+
+  onMount(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      // Cleanup listener when the component is unmounted
+      window.removeEventListener("scroll", handleScroll);
+    };
+  });
 
   function goTop() {
     document.body.scrollIntoView();
@@ -14,30 +27,30 @@
 </script>
 
 <div
-  class="dark:bg-pink-100 relative flex flex-col mx-auto w-full text-sm sm:text-base min-h-screen items-center"
+  class="bg-pink-100 dark:bg-transparent relative flex flex-col mx-auto w-full text-sm sm:text-base min-h-screen items-center"
 >
   <div
     class={"fixed bottom-0 right-0 w-full duration-200 flex p-10 z-[10] " +
-      (y > 50
+      (scrollPosition > 50
         ? " opacity-full pointer-events-auto"
         : " opacity-0 pointer-events-none")}
   >
     <button
       on:click={goTop}
-      class="ml-auto rounded-full bg-slate-900 text-violet-400 px-3 sm:px-4 hover:bg-slate-800 cursor-pointer aspect-square grid place-items-center"
+      class="ml-auto rounded-full bg-slate-900 text-lime-400 px-3 sm:px-4 hover:bg-slate-800 cursor-pointer aspect-square grid place-items-center"
     >
       <Icon icon="ri:arrow-up-circle-line" />
     </button>
   </div>
   <div class="w-full m-auto flex items-center flex-col">
-    <Header {y} />
+    <Header {scrollPosition} />
     <slot />
     <Footer />
   </div>
 </div>
 
-<svelte:window
-  bind:scrollY={y}
-  bind:innerHeight
-  bind:innerWidth
-/>
+<!--<svelte:window-->
+<!--  bind:scrollY={scrollPosition}-->
+<!--  bind:innerHeight-->
+<!--  bind:innerWidth-->
+<!--/>-->
