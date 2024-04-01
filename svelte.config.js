be7265a89539fb5769e763d7fc1e551b9e3960ee
Echoooo <echoshihtw@gmail.com>
2024-04-01 17:55:43 +0800

    ci: fallback

--- svelte.config.js
+++ svelte.config.js
@@ -17,7 +17,7 @@ const config = {
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter({
-      fallback: "200.html",
+      fallback: "index.html",
       pages: "build/", // output directory for pages
       assets: "build/", // output directory for assets
     }),
