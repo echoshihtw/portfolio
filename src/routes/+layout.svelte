e09c9eca0dc720bc458e9add63ffd7ac9ada55d6
Echoooo <echoshihtw@gmail.com>
2024-05-11 17:24:55 +0800

    feat: add projects names to mobile drawer menu

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,7 @@
   import { Drawer, initializeStores } from "@skeletonlabs/skeleton";
   import { AppShell } from "../lib/components/layout";
   import { tabs } from "$lib/menuTabs";
+  import { projects } from "$lib/projects";
 
   initializeStores();
 </script>
@@ -13,7 +14,7 @@
   rounded="rounded-l-2xl"
   width="w-[280px] md:w-[480px]"
 >
-  <div class="w-full pt-10 flex items-center gap-4 flex-col">
+  <div class="w-full p-10 flex items-start gap-4 flex-col">
     {#each tabs as tab, index}
       <a
         href={tab.link}
@@ -22,6 +23,15 @@
       >
         <p>{tab.name}</p>
       </a>
+      <div class="flex flex-col gap-6 ml-6">
+        {#if tab.name === "#projects"}
+          {#each projects as project}
+            <div class="text-gray-400 text-sm">
+              {project.name}
+            </div>
+          {/each}
+        {/if}
+      </div>
     {/each}
   </div>
 </Drawer>
