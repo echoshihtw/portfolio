dee47a7d2c6a81b3597f16d07252674eb32d46d1
Echoooo <echoshihtw@gmail.com>
2024-05-11 12:52:32 +0800

    fix: load particles on mount

--- src/lib/components/layout/AppShell.svelte
+++ src/lib/components/layout/AppShell.svelte
@@ -17,6 +17,14 @@
   });
   onMount(() => {
     window.addEventListener("scroll", handleScroll);
+    void particlesInit(async (engine) => {
+      // call this once per app
+      // you can use main to customize the tsParticles instance adding presets or custom shapes
+      // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+      // starting from v2 you can add only the features you need reducing the bundle size
+      //await loadFull(main);
+      await loadSlim(engine);
+    });
     return () => {
       // Cleanup listener when the component is unmounted
       window.removeEventListener("scroll", handleScroll);
@@ -77,15 +85,6 @@
       detectRetina: true,
     },
   };
-
-  void particlesInit(async (engine) => {
-    // call this once per app
-    // you can use main to customize the tsParticles instance adding presets or custom shapes
-    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-    // starting from v2 you can add only the features you need reducing the bundle size
-    //await loadFull(main);
-    await loadSlim(engine);
-  });
 </script>
 
 <div
